9aa494ee7785dc02964cb32de8ee8ce72ef242b3
Sachin Gurjar <98685642+SachinCoder1@users.noreply.github.com>
2023-12-04 12:51:59 +0530

    filtering function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+struct FilterCondition<T> {
+    condition_value: T,
+}
+
+impl<T: PartialEq> FilterCondition<T> {
+    fn is_match(&self, item: &T) -> bool {
+        &self.condition_value == item
+    }
+}
+
+fn custom_filter<T>(collection: Vec<T>, condition: &FilterCondition<T>) -> Vec<T>
+where
+    T: PartialEq,
+{
+    collection.into_iter().filter(|item| condition.is_match(item)).collect()
+}
+
+fn main() {
+    let collection = vec![1, 2, 3, 4, 5];
+    let condition = FilterCondition { condition_value: 3 };
+
+    let filtered_collection = custom_filter(collection, &condition);
+    println!("Filtered result: {:?}", filtered_collection);
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
